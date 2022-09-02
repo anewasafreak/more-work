f5d64b15d04cfd6e06e99b2f89b0211885cefe2d
anewasafreak <lilyte253@gmail.com>
2022-09-02 15:09:00 +0300

    Final form exercises

--- JS/8/2/script.js
+++ JS/8/2/script.js
@@ -25,7 +25,7 @@
 // function drawC(n) {
 //     const size = Number(n.target.value);
 //     let letterC = "";
-  
+
 //     let outputC = document.getElementById('C-output');
 
 //     for (let i = 0; i < size - 1; i++) {
@@ -37,19 +37,49 @@
 //     for (let i = 0; i < size; i++) {
 //       letterC += "C";
 //     }
-  
+
 //     outputC.innerHTML = letterC;
 //   }
-  
+
 //   document.getElementById("C-input").addEventListener("input", drawC);
 
 // ====== EX. 3 ======
 
-function giveName(event) {
-let name = event.target.value;
-document.body.append(name + ', ')
-}
+// function giveName(event) {
+// let name = event.target.value;
+// document.body.append(name + ', ')
+// }
 
-document.querySelector('input[type=text]').addEventListener('blur', giveName)
+// document.querySelector('input[type=text]').addEventListener('blur', giveName)
 
 // ====== EX. 4 ======
+// function compareNumbers(event) {
+//   event.preventDefault();
+//   let firstNumber = document.getElementById('n1').value
+//   let secondNumber = document.getElementById('n2').value
+//   if(Math.abs(100 - firstNumber) < Math.abs(100 - secondNumber)) {
+//     alert(firstNumber)
+//   } else {
+//     alert(secondNumber)
+//   }
+// }
+
+// document.querySelector('form').addEventListener('submit', compareNumbers)
+
+// ====== EX. 5 ======
+let randomNumber = Math.floor(Math.random() * 5 + 1);
+console.log(randomNumber);
+let counter = 0;
+
+function guessNumber(event) {
+  event.preventDefault();
+  counter++
+  guessedNumber = Number(document.querySelector("input[type=number]").value);
+  if (guessedNumber === randomNumber) {
+    alert(`You guesssed the number out of the ${counter} time!`);
+  } else {
+    alert("You didn't guess the number, try again!");
+  }
+}
+
+document.querySelector("form").addEventListener("submit", guessNumber);
